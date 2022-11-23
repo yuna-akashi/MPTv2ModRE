44da686869a483efce2b676a5e34d5aea4d6a08a
ゆな <93274135+Yunatexya@users.noreply.github.com>
2022-11-23 16:04:39 +0900

    Base content update

--- src/mptv2re/content/MPTv2Blocks.java
+++ src/mptv2re/content/MPTv2Blocks.java
@@ -7,15 +7,14 @@ import mindustry.world.Block;
 import mindustry.world.blocks.defense.Wall;
 import mindustry.world.blocks.production.GenericCrafter;
 
-import static mindustry.type.ItemStack.with;
-
 public class MPTv2Blocks {
     public static Block
         //wall
         metrenWall, metrenWallLarge,
         //turrets
-        //factory
-        metrenSmelter
+        //base factory
+        metrenSmelter, metrenFrameCrafter, hugeFrameCrafter
+        //metren's factory
     ;
 
     public static void load(){
@@ -26,21 +25,21 @@ public class MPTv2Blocks {
             craftTime = 72;
             consumePower(6);
             consumeItems(with(Items.graphite, 10,Items.lead, 15, Items.titanium, 25));
-            outputItems = with(MPTv2Items.metren, 5);
+            outputItems = with(MPTv2Items.i1_1metren, 5);
             requirements(Category.crafting, with(Items.copper, 156, Items.lead, 200, Items.titanium, 250));
         }};
 
         metrenWall = new Wall("metrenWall"){{
             size = 1;
             health = 2000000;
-            requirements(Category.defense, with(MPTv2Items.metren, 8));
+            requirements(Category.defense, with(MPTv2Items.i1_1metren, 8));
         }};
 
         metrenWallLarge = new Wall("metrenWallLarge"){{
             size = 2;
             health = 8000000;
             armor = 200f;
-            requirements(Category.defense, with(MPTv2Items.metren, 32));
+            requirements(Category.defense, with(MPTv2Items.i1_1metren, 32));
         }};
     }
 }
--- src/mptv2re/content/MPTv2Items.java
+++ src/mptv2re/content/MPTv2Items.java
@@ -6,21 +6,59 @@ import mindustry.type.Item;
 public class MPTv2Items {
     public static Item
         //materials
-        i1-1metren, i1-1metrenGlass, i1-2metrenDiamond, i1-3titaniumAlloy,
+        i1_1metren, i1_1metrenGlass, i1_2metrenDiamond, i1_2metrenSilicon,
+        i1_3titaniumAlloy, i1_4compressedThorium,
         //building materials
-        i2-1metrenFrame, i2-2hugeMetrenFrame, i2-3emperorsBuildingFrame,
-        i2-4mTurretFrame, i2-5hugeMTurretFrame, i2-6emperorsTurretFrame,
-        i2-7armorPlate, i2-8hugeArmorPlate, i2-9emperorsArmorPlate,
+        i2_1metrenFrame, i2_2hugeMetrenFrame, i2_3emperorsBuildingFrame,
+        i2_4mTurretFrame, i2_5hugeMTurretFrame, i2_6emperorsTurretFrame,
+        i2_7armorPlate, i2_8hugeArmorPlate, i2_9emperorsArmorPlate,
         //resources
-        i3-1cell, i3-2deuterium, i3-2solidOxygen, i3-3unitCore, i3-4multiCore,
-        i3-5antimatterCell,
+        i3_1cell, i3_2coolingCell, i3_2deuterium, i3_2solidOxygen, i3_3unitCore, i3_4multiCore,
+        i3_5antimatterCell,
         //ammo
-        i4-1metrenAmmo, i4-1metrenMissile, i4-2emperorsCristal
+        i4_1metrenAmmo, i4_1metrenMissile, i4_2emperorsCristal
     ;
 
     public static void load() {
-        i1-1metren = new Item("i1-1metren", Color.valueOf("4f7e81"));
-        i1-1metrenGlass = new Item("i1-1metrenGlass", Color.valueOf("aaaaaa"));
-        i1-2metrenDiamond = new Item("i1-2metrenDiamond", Color.valueOf("aaaaaa"));
+        //material
+        i1_1metren = new Item("metren", Color.valueOf("4f7e81"));
+        i1_1metrenGlass = new Item("metrenGlass", Color.valueOf("bbbbbb"));
+        i1_2metrenDiamond = new Item("metrenDiamond", Color.valueOf("5efffa"));
+        i1_2metrenSilicon = new Item("metrenSilicon",Color.valueOf("5A7E80"));
+        i1_3titaniumAlloy = new Item("titaniumAlloy", Color.valueOf("aaaaaa"));
+        i1_4compressedThorium = new Item("compressedThorium", Color.valueOf("4f7e81")){{
+            explosiveness = 0.9f;
+            radioactivity = 4f;
+            flammability = 1.5f;
+        }};
+
+        //building material
+        i2_1metrenFrame = new Item("metrenFrame", Color.valueOf("4f7e81"));
+        i2_2hugeMetrenFrame = new Item("hugeMetrenFrame", Color.valueOf("4f7e81"));
+        i2_3emperorsBuildingFrame = new Item("emperorsBuildingFrame", Color.valueOf("4f7e81"));
+        i2_4mTurretFrame = new Item("mTurretFrame", Color.valueOf("4f7e81"));
+        i2_5hugeMTurretFrame = new Item("hugeMTurretFrame", Color.valueOf("4f7e81"));
+        i2_6emperorsTurretFrame = new Item("emperorsTurretFrame", Color.valueOf("4f7e81"));
+        i2_7armorPlate = new Item("armorPlate", Color.valueOf("4f7e81"));
+        i2_8hugeArmorPlate = new Item("hugeArmorPlate", Color.valueOf("4f7e81"));
+        i2_9emperorsArmorPlate = new Item("emperorsArmorPlate", Color.valueOf("4f7e81"));
+        //resource
+        i3_1cell = new Item("cell", Color.valueOf("5b5e62"));
+        i3_2coolingCell = new Item("coolingCell", Color.valueOf("e6ff00"));
+        i3_2deuterium = new Item("deuterium", Color.valueOf("0f0f0f")){{
+            explosiveness = 70f;
+            radioactivity = 70f;
+        }};
+        i3_2solidOxygen = new Item("solidOxygen", Color.valueOf("aaaaaa")){{
+            flammability = 100000;
+        }};
+        i3_3unitCore = new Item("unitCore", Color.valueOf("aaaaaa"));
+        i3_4multiCore = new Item("multiCore", Color.valueOf("ffdc62"));
+        i3_5antimatterCell = new Item("antimatterCell", Color.valueOf("0000000"));
+
+        //ammo
+        i4_1metrenAmmo = new Item("metrenAmmo", Color.valueOf("4f7e81"));
+        i4_1metrenMissile = new Item("metrenMissile", Color.valueOf("4f7e81"));
+        i4_2emperorsCristal = new Item("emperorsCristal", Color.valueOf("FFFFA0"));
     }
 }
